513ffb74ef2f47202123db13c832c7834715c269
Loading… <70993217+zdisanto@users.noreply.github.com>
2024-03-19 08:44:56 -0400

    Updating Visuals

--- commands.js
+++ commands.js
@@ -2,7 +2,7 @@ help = [
     'about      Who am I?',
     'resume     My CV',
     'blog       View my tailored thoughts',
-    'notes      My notes on school/learning/cpb',
     'favorites  Websites, books, you name it...',
+    'notes      My notes on school/learning/cpb',
     'help       If you need it, just ask'
 ];
